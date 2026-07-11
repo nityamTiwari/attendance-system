43fc5810f461695892ae0974243e5eb21df440ee
Nityam Tiwari <nityam.tiwari@virtubox.io>
2026-07-11 18:26:18 +0530

    Initial Spring Boot Project setup

---
+++ src/test/java/com/nityam/attendance_system/AttendanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nityam.attendance_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
