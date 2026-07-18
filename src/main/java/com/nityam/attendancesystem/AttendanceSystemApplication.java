e66ffca2d9810ba2f68edfa8bf764ab0ef93b390
Nityam Tiwari <nityam.tiwari@virtubox.io>
2026-07-19 01:01:39 +0530

    make main class for spring boot

--- src/main/java/com/nityam/attendancesystem/AttendanceSystemApplication.java
+++ src/main/java/com/nityam/attendancesystem/AttendanceSystemApplication.java
@@ -1,10 +1,10 @@
-package com.nityam.attendance_system;
+package com.nityam.attendancesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class AttendanceSystemApplication {
 
 	public static void main(String[] args) {
