0254e4c2f45dbade2aa28e5e0b490273f6ebbf44
Nityam Tiwari <nityam.tiwari@virtubox.io>
2026-07-19 00:59:43 +0530

    rename project name and package

--- src/main/java/com/nityam/attendancesystem/AttendanceSystemApplication.java
+++ src/main/java/com/nityam/attendancesystem/AttendanceSystemApplication.java
@@ -2,8 +2,9 @@ package com.nityam.attendance_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AttendanceSystemApplication {
 
 	public static void main(String[] args) {
