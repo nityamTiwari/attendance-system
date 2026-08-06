e0fb51e268715516047851115799117471d97d3b
Nityam Tiwari <nityamtiwari08@gmail.com>
2026-08-06 23:39:03 +0530

    add controller today attendance

--- src/main/java/com/nityam/attendancesystem/attendance/controller/AttendanceController.java
+++ src/main/java/com/nityam/attendancesystem/attendance/controller/AttendanceController.java
@@ -4,9 +4,11 @@ package com.nityam.attendancesystem.attendance.controller;
 import com.nityam.attendancesystem.attendance.dto.AttendanceResponse;
 import com.nityam.attendancesystem.attendance.dto.ClockInResponse;
 import com.nityam.attendancesystem.attendance.dto.ClockOutResponse;
+import com.nityam.attendancesystem.attendance.dto.TodayAttendanceResponse;
 import com.nityam.attendancesystem.attendance.service.AttendanceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +39,12 @@ public class AttendanceController {
     public ResponseEntity<?> attendanceHistory(){
         List<AttendanceResponse> response =  attendanceService.attendanceHistory();
         return  ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/today")
+    public ResponseEntity<TodayAttendanceResponse> todayAttendance(){
+
+        return ResponseEntity.ok(attendanceService.getTodayAttendance());
     }
 
 }
